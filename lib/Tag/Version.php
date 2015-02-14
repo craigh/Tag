f11999e073cf4106b40e6ccd024dda0cb303a7f3
Craig Heydenburg <craigh@mac.com>
2015-02-14 11:50:32 -0500

    update version number to 1.0.3

--- lib/Tag/Version.php
+++ lib/Tag/Version.php
@@ -25,7 +25,7 @@ class Tag_Version extends Zikula_AbstractVersion
         $meta['displayname'] = $this->__('Tag');
         $meta['description'] = $this->__("Tagging module");
         $meta['url'] = $this->__('tag');
-        $meta['version'] = '1.0.3-dev';
+        $meta['version'] = '1.0.3';
         $meta['core_min'] = '1.3.3'; // requires minimum 1.3.3 or later
         $meta['core_max'] = '1.3.99'; // doesn't work with later branches
         $meta['securityschema'] = array('Tag::' => '::');
